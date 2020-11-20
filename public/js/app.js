38eb45171affa45d916ce4ad0f5f2c1800de4693
MatthewHightech <Smoose007@gmail.com>
2020-11-19 16:50:33 -0800

    embedded video and default to Q1

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,13 @@ let lastElement;
 // Function: Sets the html of the id="text" element based on which question is asked, identified by the param
 // Param: id for specified question
 function question(q, element) {
-     
+    // if a last element exists, set it back to the unselected color of dark grey. 
     if (lastElement != null) {
         lastElement.style.background = "#222428";
     }
-    lastElement = element;
 
     let content = ""; 
+    // based on the id passed to the function, dynamically change the output of the answer box
     switch(q) {
         case 1: content = "Working for HighTechU would give me a chance to further help my local community by working with a team that passionately supports Victoria's youth pursuing a future in technology. Having recently grown in my own career as a software developer, I feel well-qualified to provide high school students with the relevant tools to put them on a path of success. Not only will this position build my own skills as a developer, but give me a platform to equip future innovators."; break; 
         case 2: content = "Due to my limited course load at the moment (having only Physics 12) I would be able to dedicate the full 20 hours, most weeks from November 30th to December 18th (though I couldn't work/meet between 9 and 11:30 am). After Dec. 18th, I would be able to consistently deliver anything up to the maximum of 20 hours a week since I'll no longer be in school. "; break; 
@@ -16,7 +16,10 @@ function question(q, element) {
         case 4: content = "From the self-reflecting I've done in the past, I forsee a balanced schedule being the most difficult aspect of taking on this position. I'm not worried about after Dec. 18th when I finish school, but until then, it will take some extra planning on my part to stay on top of my schooling and the internship. That being said, I am up to the challenge and know that this will be a great opportunity to practice my time-management for later seasons of life such as university studies or full-time work."; break; 
         case 5: content = "In the case that this internship doesn't work out, I would be encouraged to meet with whoever assesses the applications and welcome constructive criticism on how I could improve for the next time I look for a similar opportunity. Next, I'd take the hours otherwise spent on the internship to improve my technical skills and grow my network to be fully prepared and open as many doors as possible for future work prospects."; break; 
         case 6: content = "Ever since I was little I've been intrigued by innovative solutions to everyday problems, and as soon as I was introduced to programming, I felt like the doors to possibility had been blown open. Every time I sit down at my laptop, I get butterflies in my stomach by just knowing that I can build anything. I view programming as a creative art, painting my Monet one line at a time. Having the fluency to transform my ideas into solutions to complex problems, brings meaning and joy to my life."; break; 
+        default: element = document.getElementById("item1"); content = "Working for HighTechU would give me a chance to further help my local community by working with a team that passionately supports Victoria's youth pursuing a future in technology. Having recently grown in my own career as a software developer, I feel well-qualified to provide high school students with the relevant tools to put them on a path of success. Not only will this position build my own skills as a developer, but give me a platform to equip future innovators."; break;  
     } // switch
     document.getElementById("text").innerHTML = content;
-    element.style.background = "#383a3e";  
+    element.style.background = "#383a3e"; 
+    // set the last element to the current element
+    lastElement = element; 
 } // question
